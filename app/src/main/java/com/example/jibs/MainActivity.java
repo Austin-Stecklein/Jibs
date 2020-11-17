fdaf71bc3391d7f1271ee71f14450e4191d493ad
Austin-Stecklein <ste18088@byui.edu>
2020-11-16 20:33:22 -0700

    Basic Calendar view added

--- app/src/main/java/com/example/jibs/MainActivity.java
+++ app/src/main/java/com/example/jibs/MainActivity.java
@@ -18,14 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LocalDate date = LocalDate.now();
-        LocalDate date1 = date.withMonth(1);
-        DayOfWeek week = date1.getDayOfWeek();
-        Log.i("My Tag", String.valueOf(date1));
-        Log.i("Day of week", String.valueOf(week));
+        
 
-        TextView textView = (TextView)findViewById(R.id.Date);
-        textView.setText(String.valueOf(week));
+
+
 
     }
 }
