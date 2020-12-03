157c78a40afab7280287d9e605c130653dd97167
Austin-Stecklein <ste18088@byui.edu>
2020-12-03 15:03:00 -0700

    Added switch to the holidayinfo activity.

--- app/src/main/java/com/example/jibs/HolidayInfo.java
+++ app/src/main/java/com/example/jibs/HolidayInfo.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Switch;
 import android.widget.TextView;
 
 //This is the activity that is called when you click on an item in the list.
@@ -19,13 +20,21 @@ public class HolidayInfo extends AppCompatActivity {
         String name = intent.getStringExtra("Name");
         String date = intent.getStringExtra("Date");
         String description = intent.getStringExtra("Description");
+        String notification = intent.getStringExtra("Notification");
+        boolean notif = false;
+
+        if(notification == "true") {
+            notif = true;
+        }
 
         TextView textView = (TextView) findViewById(R.id.HolidayName);
         TextView textView1 = (TextView) findViewById(R.id.HolidayDate);
         TextView textView2 = (TextView) findViewById(R.id.HolidayDescription);
+        Switch simpleSwitch = (Switch) findViewById(R.id.notfications);
 
         textView.setText(name);
         textView1.setText(date);
         textView2.setText(description);
+        simpleSwitch.setChecked(notif);
     }
 }
