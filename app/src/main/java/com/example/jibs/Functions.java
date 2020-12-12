60444c973139c8cd7e4b2e4ebf2032a50066e2db
Austin-Stecklein <ste18088@byui.edu>
2020-12-12 13:27:52 -0700

    Revert "Added comments to the function class"
    This reverts commit f5f9e630ebd682b8a7a7cb4e37a6caef4af39058.

--- app/src/main/java/com/example/jibs/Functions.java
+++ app/src/main/java/com/example/jibs/Functions.java
@@ -6,6 +6,7 @@ import androidx.annotation.RequiresApi;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,13 +16,16 @@ import java.util.ArrayList;
 
 public class Functions {
 
-    // This grabs the url for the API based on the month that the holidays are in
     public URL getMonth(String apID, String country, int year, int month) throws IOException {
         URL url = new URL("https://holidays.abstractapi.com/v1/?api_key=" + apID + "&country=" + country + "&year=" + year + "&month=" + month);
         return url;
     }
 
-    // This function takes the url and places it into the API to gather the data into an array list
+    public URL getDay(String apID, String country, int year, int month, int day) throws IOException {
+        URL url = new URL("https://holidays.abstractapi.com/v1/?api_key=" + apID + "&country=" + country + "&year=" + year + "&month=" + month + "&day=" + day);
+        return url;
+    }
+
     public HolidayItem[] getData(URL url) throws IOException {
         HttpURLConnection urlConnect = (HttpURLConnection) url.openConnection();
 
@@ -35,7 +39,6 @@ public class Functions {
         return hList;
     }
 
-    // This goes into the json file and filters through the holidays by month to allow the data connection to grab the holidays into the appropriate month
     @RequiresApi(api = Build.VERSION_CODES.O)
     public HolidayItem[] readJson(Context context, int month) throws IOException {
         String fileName = "funHolidays.json";
@@ -49,7 +52,6 @@ public class Functions {
 
         jsonString = new String(buffer, "UTF-8");
 
-        // This filters the json into months for the data connection
         Gson gson = new Gson();
             HolidayItem[] hList = gson.fromJson(jsonString, HolidayItem[].class);
             ArrayList<HolidayItem> hList2 = new ArrayList<HolidayItem>();
